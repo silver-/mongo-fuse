163a6e5e43696d3ea37a149c77b4869bb4ff6f93
silver <silver@silver-laptop.(none)>
2012-06-30 17:16:39 +0300

    Added split_path function.

--- tests/test_mongofuse.py
+++ tests/test_mongofuse.py
@@ -1,17 +1,21 @@
+# Standard modules:
 import unittest
+import stat
+
+# Third-party modules:
 import pymongo
 import mongofuse
 
 TEST_DB = "localhost:27017"
 
 
-class MongoFuseTest(unittest.TestCase):
+class RepresentDatabasesAsFoldersTest(unittest.TestCase):
 
     def setUp(self):
         self.conn = pymongo.Connection(TEST_DB, safe=True)
         self.fuse = mongofuse.MongoFuse(conn_string=TEST_DB)
 
-    def test_should_represent_databases_as_folders(self):
+    def test_readdir(self):
 
         # Given number of MongoDB databases
         db1 = self.conn['test_1']
@@ -37,6 +41,45 @@ class MongoFuseTest(unittest.TestCase):
         # And special "." and ".." folders should be listed as well
         self.assertIn(".", readdir)
         self.assertIn("..", readdir)
+
+    def test_getattr(self):
+
+        # Given a MongoDB database
+        db = self.conn['test_1']
+        db.test.insert({'db': "test_1"})
+
+        # When getting attributes for the database folder entry
+        attrs = self.fuse.getattr('/test_1')
+
+        # Then folder flag should be set
+        self.assertTrue(stat.S_ISDIR(attrs['st_mod']))
+
+
+class SplitPathTest(unittest.TestCase):
+
+    def test_should_split_path_into_list_of_components(self):
+
+        # Given a path
+        path = "/tmp/test/my_file.json"
+
+        # When applying split_path() to it
+        components = mongofuse.split_path(path)
+
+        # Then list of individual components should be returned
+        self.assertEqual(components, ["/", "tmp", "test", "my_file.json"])
+
+    def test_should_normalize_path(self):
+
+        # Given a non-normalized path
+        path = "/tmp/test/"
+
+        # When applying split_path() to it
+        components = mongofuse.split_path(path)
+
+        # Then list of individual components should be returned
+        self.assertEqual(components, ["/", "tmp", "test"])
+
+
 
 
 if __name__ == '__main__':
